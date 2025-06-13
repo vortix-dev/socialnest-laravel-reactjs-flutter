4802723ca828b54ff88b29e292236ec5811f9e11
vortix-dev <vortixdev@gmail.com>
2025-06-13 06:58:19 +0100

    create database migation & make controller authentification

--- backend/database/migrations/0001_01_01_000000_create_users_table.php
+++ backend/database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,6 +14,10 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('username')->unique();
+            $table->text('bio');
+            $table->date('date_de_naissance');
+            $table->enum('sexe',['male','female']);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
