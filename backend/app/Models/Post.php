2ec6ff2cd0f5bc959c34108eb2670b4359b965de
vortix-dev <vortixdev@gmail.com>
2025-07-05 04:20:20 +0100

    post commit

--- backend/app/Models/Post.php
+++ backend/app/Models/Post.php
@@ -11,7 +11,6 @@ class Post extends Model
 
     protected $fillable = [
         'user_id',
-        'post',
         'description',
         'date',
         'heure',
--- backend/database/migrations/2025_07_05_030432_create_posts_table.php
+++ backend/database/migrations/2025_07_05_030432_create_posts_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained('users');
-            $table->string('post');
             $table->text('description');
             $table->date('date');
             $table->time('heure');
