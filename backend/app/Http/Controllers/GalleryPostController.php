a441eb54ea794b9a1add2845cb9aab322bf3a086
vortix-dev <vortixdev@gmail.com>
2025-07-11 07:57:23 +0100

    migration commit

---
+++ backend/app/Http/Controllers/GalleryPostController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\GalleryPost;
+use Illuminate\Http\Request;
+
+class GalleryPostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(GalleryPost $galleryPost)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(GalleryPost $galleryPost)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, GalleryPost $galleryPost)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(GalleryPost $galleryPost)
+    {
+        //
+    }
+}
---
+++ backend/app/Models/GalleryPost.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class GalleryPost extends Model
+{
+    //
+}
